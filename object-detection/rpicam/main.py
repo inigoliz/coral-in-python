8b9a8f2e70ce62b81fa4cd7a06b4dcb0744ef461
Iñigo Lara <ignigoliz@gmail.com>
2025-01-31 16:51:08 +0100

    debig video feed rpi

--- object-detection/rpicam/main.py
+++ object-detection/rpicam/main.py
@@ -5,18 +5,19 @@ def main():
     model_w, model_h = 300, 300
 
     with Picamera2() as picam2:
-        main = {'size': (video_w, video_h), 'format': 'XRGB8888'}
-        lores = {'size': (model_w, model_h), 'format': 'RGB888'}
-        controls = {'FrameRate': 30}
-        config = picam2.create_preview_configuration(main, lores=lores, controls=controls)
-        picam2.configure(config)
+        # main = {'size': (video_w, video_h), 'format': 'XRGB8888'}
+        # lores = {'size': (model_w, model_h), 'format': 'RGB888'}
+        # controls = {'FrameRate': 30}
+        # config = picam2.create_preview_configuration(main, lores=lores, controls=controls)
+        # picam2.configure(config)
 
         picam2.start_preview(Preview.QTGL, x=0, y=0, width=video_w, height=video_h)
         picam2.start()
         # picam2.pre_callback = draw_objects
 
         # Process each low resolution camera frame.
-        # while True:
+        while True:
+            print('hi')
         #     frame = picam2.capture_array('lores')
 
         #     # Run inference on the preprocessed frame
