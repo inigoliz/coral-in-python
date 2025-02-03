aef9afdb80bb5ecb02486826ffbba400a44f6f42
Iñigo Lara <ignigoliz@gmail.com>
2025-02-03 12:21:29 +0100

    I cannot use lowres

--- rpi/test_video.py
+++ rpi/test_video.py
@@ -6,7 +6,7 @@ import numpy as np
 import time
 
 def draw_objects(request):
-    with MappedArray(request, "lores") as m:
+    with MappedArray(request, "main") as m:
         # Convert the array to a PIL Image
         image = Image.fromarray(m.array)
         # print(image.size)
@@ -28,14 +28,14 @@ def draw_objects(request):
 
 # Configure and start Picamera2.
 picam2 = Picamera2()
-video_w, video_h = 640, 480
+video_w, video_h = 300, 300
 main = {'size': (video_w, video_h), 'format': 'XRGB8888'}
-lores = {'size': (300, 300), 'format': 'BGR888'}
+# lores = {'size': (300, 300), 'format': 'BGR888'}
 controls = {'FrameRate': 30}
-config = picam2.create_preview_configuration(main, lores=lores, controls=controls)
+config = picam2.create_preview_configuration(main, controls=controls)
 picam2.configure(config)
 
-picam2.start_preview(Preview.QTGL, transform=Transform(vflip=1))
+picam2.start_preview(Preview.QTGL, width=video_w, height=video_h, transform=Transform(vflip=1))
 picam2.start()
 picam2.pre_callback = draw_objects
 
